464d565858a570b0365c903cd022bb71659ec279
Brochu <alexbrochu1@gmail.com>
2024-08-27 01:22:31 -0400

    [NOTES] Next steps, keep track as I read Tracy' src code

--- main.c
+++ main.c
@@ -2,5 +2,9 @@
 
 int main(int argc, char **argv) {
     printf("[CTracy] starting program\n");
+    //TODO: First thing is to setup UDP broadcast socket
+    //Look into WorkerThread to see how they do it
+    //
+    //First Goal, Broadcast new app + Go through handshake process
     return 0;
 }
